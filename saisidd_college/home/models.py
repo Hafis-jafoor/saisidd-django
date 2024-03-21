8b889cfe9e4a9af41390c461dbe992956a701190
Hafis-jafoor <hafis3052000@gmail.com>
2024-03-21 17:35:53 +0530

    21/03/2024

--- saisidd_college/home/models.py
+++ saisidd_college/home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class contacts(models.Model):
+    contact_name  = models.CharField(max_length=255)
+    contact_email = models.EmailField(unique = True)
+    contact_phone = models.CharField(max_length=10)
+    contact_subject = models.CharField(max_length=100)
+    contact_message = models.TextField()
